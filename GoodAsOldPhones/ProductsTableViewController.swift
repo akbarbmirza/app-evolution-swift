7d52cd6410e678f240a00b8507fc8f4e00ca10f3
Akbar Mirza <akbarbmirza@gmail.com>
2016-12-27 12:05:56 -0500

    Refactored Code with Product Class

--- GoodAsOldPhones/ProductsTableViewController.swift
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class ProductsTableViewController: UITableViewController {
     
-    var productNames: [String]?
+    var products: [Product]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        productNames = [
-            "1907 Wall Set", "1921 Dial Phone",
-            "1937 Desk Set", "1984 Motorola Portable"
-        ]
+        let product1 = Product()
+        let product2 = Product()
+        let product3 = Product()
+        let product4 = Product()
+        
+        product1.name = "1907 Wall Set"
+        product1.productImage = "phone-fullscreen1"
+        product1.cellImage = "image-cell1"
+        
+        product2.name = "1921 Dial Phone"
+        product2.productImage = "phone-fullscreen2"
+        product2.cellImage = "image-cell2"
+        
+        product3.name = "1937 Desk Set"
+        product3.productImage = "phone-fullscreen3"
+        product3.cellImage = "image-cell3"
+        
+        product4.name = "1984 Motorola Portable"
+        product4.productImage = "phone-fullscreen4"
+        product4.cellImage = "image-cell4"
+        
+        products = [product1, product2, product3, product4]
     }
     
     // The Table View Controller suns some table-specific functions
@@ -28,8 +46,8 @@ class ProductsTableViewController: UITableViewController {
         tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
         
-        if let pNames = productNames {
-            return pNames.count
+        if let p = products {
+            return p.count
         }
         
         return 0
@@ -41,14 +59,17 @@ class ProductsTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell",
                                                  for: indexPath)
-        
-        let productName = productNames?[indexPath.row] // set the product name
+        let product = products?[indexPath.row] // set the product name
             
-        if let pName = productName {
-            cell.textLabel?.text = pName
+        if let p = product {
+            cell.textLabel?.text = p.name
+            if let i = p.cellImage {
+                cell.imageView?.image = UIImage(named: i)
+            }
+            
+
         }
         
-        cell.imageView?.image = #imageLiteral(resourceName: "image-cell1")
         
         return cell
     }
@@ -66,7 +87,7 @@ class ProductsTableViewController: UITableViewController {
             
             // use the index path to get the productName from the array
             // send the product name to the product view controller
-            productVC?.productName = productNames?[indexPath.row]
+            productVC?.product = products?[indexPath.row]
         }
     }
 
